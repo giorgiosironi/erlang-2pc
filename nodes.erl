7436611be7e222cce2dba460e61a9737cd1f3bde
Giorgio Sironi <info@giorgiosironi.com>
2014-12-07 13:20:40 +0100

    Processes can now have a proposed value for consensus (commit or abort basically)

--- nodes.erl
+++ nodes.erl
@@ -1,16 +1,20 @@
 -module(nodes).
--export([setup/0, start/0, one_node/2]).
+-export([setup/0, start/0, one_node/0]).
 
+-record(node_state, {value}).
+
+one_node() -> one_node([], #node_state{value=nil}).
 one_node(Cohorts, State) -> receive
         {add_cohort, Pid} -> one_node(lists:append(Cohorts, [Pid]), State);
-        {propose_value, Value} -> one_node(Cohorts, State)
+        {propose_value, Value} -> one_node(Cohorts, #node_state{value=Value})
     end.
 
 setup() ->
-    A = spawn(nodes, one_node, [[], nil]),
-    B = spawn(nodes, one_node, [[], nil]),
-    C = spawn(nodes, one_node, [[], nil]),
+    A = spawn(nodes, one_node, []),
+    B = spawn(nodes, one_node, []),
+    C = spawn(nodes, one_node, []),
     A ! {add_cohort, B},
     A ! {add_cohort, C},
+    A ! {propose_value, commit}, 
     nothing.
 start() -> nothing.
