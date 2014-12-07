188f4e58747a03710c128de9c566db4b6e9a2aa7
Giorgio Sironi <info@giorgiosironi.com>
2014-12-07 13:16:44 +0100

    Adding a supported message: proposed value

--- nodes.erl
+++ nodes.erl
@@ -1,14 +1,15 @@
 -module(nodes).
--export([setup/0, start/0, one_node/1]).
+-export([setup/0, start/0, one_node/2]).
 
-one_node(Cohorts) -> receive
-        {add_cohort, Pid} -> one_node(lists:append(Cohorts, [Pid]))
+one_node(Cohorts, State) -> receive
+        {add_cohort, Pid} -> one_node(lists:append(Cohorts, [Pid]), State);
+        {propose_value, Value} -> one_node(Cohorts, State)
     end.
 
 setup() ->
-    A = spawn(nodes, one_node, [[]]),
-    B = spawn(nodes, one_node, [[]]),
-    C = spawn(nodes, one_node, [[]]),
+    A = spawn(nodes, one_node, [[], nil]),
+    B = spawn(nodes, one_node, [[], nil]),
+    C = spawn(nodes, one_node, [[], nil]),
     A ! {add_cohort, B},
     A ! {add_cohort, C},
     nothing.
