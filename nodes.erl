09f374c0bca079c171a25f5e9c67e6b2150e5a6c
Giorgio Sironi <info@giorgiosironi.com>
2014-12-07 13:30:23 +0100

    The decision of committing is pigged back to the Coordinator with agreement messages

--- nodes.erl
+++ nodes.erl
@@ -7,7 +7,11 @@ one_node() -> one_node([], #node_state{value=nil}).
 one_node(Cohorts, State) -> receive
         {add_cohort, Pid} -> one_node(lists:append(Cohorts, [Pid]), State);
         {propose_value, Value} -> one_node(Cohorts, #node_state{value=Value});
-        {start_2pc} -> query_to_commit(Cohorts)
+        {start_2pc} -> query_to_commit(Cohorts);
+        {query, Coordinator} -> if
+            State#node_state.value == commit -> Coordinator ! {agreement, yes}
+        end,
+        one_node(Cohorts, State)
     end.
 
 query_to_commit(OtherNodes) ->
@@ -19,7 +23,6 @@ setup() ->
     C = spawn(nodes, one_node, []),
     A ! {add_cohort, B},
     A ! {add_cohort, C},
-    A ! {propose_value, commit}, 
     B ! {propose_value, commit}, 
     C ! {propose_value, commit}, 
     A ! {start_2pc},
