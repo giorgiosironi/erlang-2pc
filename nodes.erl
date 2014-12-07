1140875ef9123c3ac1c331ed9ff325e09bf23053
Giorgio Sironi <info@giorgiosironi.com>
2014-12-07 13:33:22 +0100

    Dividing node types into coordinator/0 and cohort/0

--- nodes.erl
+++ nodes.erl
@@ -1,30 +1,35 @@
 -module(nodes).
--export([setup/0, start/0, one_node/0]).
+-export([setup/0, start/0, coordinator/0, cohort/0]).
 
 -record(node_state, {value}).
 
-one_node() -> one_node([], #node_state{value=nil}).
-one_node(Cohorts, State) -> receive
-        {add_cohort, Pid} -> one_node(lists:append(Cohorts, [Pid]), State);
-        {propose_value, Value} -> one_node(Cohorts, #node_state{value=Value});
-        {start_2pc} -> query_to_commit(Cohorts);
+
+coordinator() -> coordinator([], #node_state{value=nil}).
+coordinator(Cohorts, State) -> receive
+        {add_cohort, Pid} -> coordinator(lists:append(Cohorts, [Pid]), State);
+        {start_2pc_with_commit} -> query_to_commit(Cohorts)
+    end.
+
+cohort() -> cohort([], #node_state{value=nil}).
+cohort(Cohorts, State) -> receive
+        {propose_value, Value} -> cohort(Cohorts, #node_state{value=Value});
         {query, Coordinator} -> if
             State#node_state.value == commit -> Coordinator ! {agreement, yes}
         end,
-        one_node(Cohorts, State)
+        cohort(Cohorts, State)
     end.
 
 query_to_commit(OtherNodes) ->
     lists:map(fun(Node) -> Node ! {query, self()} end, OtherNodes).
 
 setup() ->
-    A = spawn(nodes, one_node, []),
-    B = spawn(nodes, one_node, []),
-    C = spawn(nodes, one_node, []),
+    A = spawn(nodes, coordinator, []),
+    B = spawn(nodes, cohort, []),
+    C = spawn(nodes, cohort, []),
     A ! {add_cohort, B},
     A ! {add_cohort, C},
     B ! {propose_value, commit}, 
     C ! {propose_value, commit}, 
-    A ! {start_2pc},
+    A ! {start_2pc_with_commit},
     nothing.
 start() -> nothing.
